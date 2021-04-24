1c1b53d77765d55df1aff319eced42913bd88bc0
ubermanu <manu@paca.happy-dev.fr>
2021-04-24 16:31:01 +0200

    Add real world example with some tests Install es version of lodash Update version of max-validator (with es support)

---
+++ example/Library.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { writable } from 'svelte/store'
+    import books, { createBook, updateBook, deleteBook, findBookByAuthor } from './books'
+
+    const bookForm = writable({
+        title: '',
+        author: ''
+    })
+</script>
+
+<form on:submit|preventDefault={() => createBook($bookForm)}>
+    <input type="text" name="title" bind:value={$bookForm.title}>
+    <input type="text" name="author" bind:value={$bookForm.author}>
+</form>
+
+<ul>
+    {#each $books as book}
+        <li>
+            <p class="title">{book.title}</p>
+            <p class="author"><small>from: {book.author}</small></p>
+        </li>
+    {/each}
+</ul>
---
+++ example/Library.test.js
@@ -0,0 +1,5 @@
+import { mount } from 'dainte'
+
+test('create a book', async () => {
+  const { window, document } = await mount('./example/Library.svelte')
+})
